b7950e3a0316edfa64da70c0d61e4fff7fb1f754
Yashwanth2008 <vyashwanth2003@gmail.com>
2024-12-01 12:46:10 +0530

    Rephaser changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function Gemini_API() {
     );
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     try {
-      const result = await model.generateContent(prompt);
+      const result = await model.generateContent(`Rephase or correct this sentence :: ${prompt}`);
       setResponse(result.response.text());
     } catch (error) {
       console.error("Error generating content:", error);
